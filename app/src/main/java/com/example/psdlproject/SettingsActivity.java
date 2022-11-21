6adf61eb04dd50e47255047efe2c6ddb61f28c57
naiksneha <SNEHA.NAIK@studentambassadors.com>
2022-11-21 20:37:21 +0530

    PSDL project latest

--- app/src/main/java/com/example/psdlproject/SettingsActivity.java
+++ app/src/main/java/com/example/psdlproject/SettingsActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.psdlproject.databinding.ActivitySettingsBinding;
+import com.example.psdlproject.databinding.ActivitySignInBinding;
+
 public class SettingsActivity extends AppCompatActivity {
 
+    ActivitySettingsBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        binding = ActivitySettingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         getSupportActionBar().hide();
     }
