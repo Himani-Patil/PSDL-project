d4888ac7b6247a0cc9a4c3624288e9e38e7f2f78
naiksneha <SNEHA.NAIK@studentambassadors.com>
2022-11-21 22:00:18 +0530

    PSDL project latest

--- app/src/main/java/com/example/psdlproject/SettingsActivity.java
+++ app/src/main/java/com/example/psdlproject/SettingsActivity.java
@@ -2,14 +2,24 @@ package com.example.psdlproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.psdlproject.databinding.ActivitySettingsBinding;
 import com.example.psdlproject.databinding.ActivitySignInBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SettingsActivity extends AppCompatActivity {
 
     ActivitySettingsBinding binding;
+    FirebaseDatabase database;
+    FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +27,35 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         getSupportActionBar().hide();
+
+        database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(SettingsActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        binding.saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!binding.txtStatus.getText().toString().equals("") && !binding.txtUsername.getText().toString().equals("")){
+                    String status = binding.txtStatus.getText().toString();
+                    String username = binding.txtUsername.getText().toString();
+
+                    HashMap<String, Object> obj = new HashMap<>();
+                    obj.put("userName", username);
+                    obj.put("status", status);
+
+                    database.getReference().child("Users").child(auth.getUid()).updateChildren(obj);
+                    Toast.makeText(SettingsActivity.this, "Profile updated Successfully", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(SettingsActivity.this, "Enter Details", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
